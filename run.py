a6bfc09851edc8eeeecf6727e527137a4c54ba7b
esir <esirkings@gmail.com>
2020-11-03 08:21:40 +0300

    Enable use of self hosted mediacloud backend

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from werkzeug.serving import run_simple
 from server import app
 
 if __name__ == '__main__':
-    run_simple('localhost', 5000, app, use_reloader=True, use_debugger=True)
+    run_simple('0.0.0.0', 80, app, use_reloader=True, use_debugger=True)
